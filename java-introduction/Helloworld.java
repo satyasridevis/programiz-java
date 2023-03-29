a7246c770237019232dbcdea38a9c201990fef4b
satyasridevis <satyasridevisk@gmail.com>
2023-03-29 10:10:26 +0530

    Course: programmiz java

---
+++ java-introduction/Helloworld.java
@@ -0,0 +1,12 @@
+public class Helloworld
+{
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // Print Number
+        System.out.println(33);
+        System.out.println("java is easy");
+        
+        
+     }
+}
